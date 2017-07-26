f923ba355b5b31994d62f5cd5cb9efbc83e359b8
Lasse Vågsæther Karlsen <lassevagsaether.karlsen@visma.com>
2017-07-26 15:23:15 +0200

    Moved C# fragment resolver to own namespace

---
+++ AutoMerge/Resolvers/DotNet/VisualStudio/Fragments/CSharpUsingStatementsInTheSamePlaceFragmentConflictResolver.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace AutoMerge.Resolvers.DotNet.VisualStudio.Fragments
+{
+    public class CSharpUsingStatementsInTheSamePlaceFragmentConflictResolver : IFragmentConflictResolver
+    {
+        public IEnumerable<string> TryResolve(IList<string> commonBase, IList<string> left, IList<string> right)
+        {
+            var re = new Regex(@"^\s*using\s+[^(]");
+            if (commonBase.Count == 0 && left.All(line => re.IsMatch(line.NotNull())) && right.All(line => re.IsMatch(line.NotNull())))
+                return left.Concat(right).Distinct();
+
+            return null;
+        }
+
+        public string[] SupportedExtensions => new[] { ".cs" };
+        public string Description => "using statements in the same place";
+    }
+}
